6f2222c96a8de69635987fa2debf5e7ab327973c
Janna <jannakojedubova@gmail.com>
2021-03-08 13:51:37 +0000

    no message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/index';
+import Categories from './components/Categories/index';
+import Banner from './components/Banner/index';
+import Supermarkets from './components/Supermarkets/index';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Categories />
+      <Banner />
+      <Supermarkets />
     </div>
   );
 }
---
+++ src/components/Supermarkets/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import './style.scss';
+
+class Supermarkets extends React.Component {
+  render(){
+    return(
+      <div className='secondwrapper'>
+        <div className='wrappertitle'>
+          <div className='supermarket_title'>Supermercados</div>
+          <div className='seeall_supermarkets'>Ver todos</div>
+        </div>
+        <div className='supermarketswrapper'>
+          <div className='supermarket'>
+            <div className='continente'></div>
+            <div className='label_descriptiption'>
+              <div className='supermarket_name'>Continente</div>
+              <div className='supermarket_description'>500 descontos</div>
+            </div>
+          </div>
+          <div className='supermarket'>
+            <div className='pingodoce'></div>
+            <div className='label_descriptiption'>
+              <div className='supermarket_name'>Pingo Doce</div>
+              <div className='supermarket_description'>500 descontos</div>
+            </div>
+          </div>
+          <div className='supermarket'>
+            <div className='lidl'></div>
+            <div className='label_descriptiption'>
+              <div className='supermarket_name'>Lidl</div>
+              <div className='supermarket_description'>500 descontos</div>
+            </div>
+          </div>
+          <div className='supermarket'>
+            <div className='aldi'></div>
+            <div className='label_descriptiption'>
+              <div className='supermarket_name'>Aldi</div>
+              <div className='supermarket_description'>500 descontos</div>
+            </div>
+          </div>
+          <div className='supermarket'>
+            <div className='auchan'></div>
+            <div className='label_descriptiption'>
+              <div className='supermarket_name'>Auchan</div>
+              <div className='supermarket_description'>500 descontos</div>
+            </div>
+          </div>
+          <div className='supermarket'>
+            <div className='minipreco'></div>
+            <div className='label_descriptiption'>
+              <div className='supermarket_name'>Mini Preço</div>
+              <div className='supermarket_description'>500 descontos</div>
+            </div>
+          </div>
+          <div className='supermarket'>
+            <div className='intermache'></div>
+            <div className='label_descriptiption'>
+              <div className='supermarket_name'>Intermaché</div>
+              <div className='supermarket_description'>500 descontos</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default Supermarkets;
