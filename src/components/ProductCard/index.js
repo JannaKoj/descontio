3922b6fdce1b61f8ff673e478c2648cf9c42ec79
Janna <jannakojedubova@gmail.com>
2021-03-15 18:35:06 +0000

    product card changes + resolver small bugs

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,28 +1,37 @@
 import React from 'react';
 import './style.scss';
+import data from '../../database.js';
 
 class ProductCard extends React.Component {
   render(){
     return(
-      <div className='productcardwrapper'>
-        <div className='producticon'></div>
-        <div className='productdescription'>
-            <div className='untildate'></div>
-            <div className='productondiscount'></div>
-            <div className='units'>
-              <div className='productunits'></div>
-              <div className='priceperunit'></div>
+      <div className='mainwrapper'>
+      {
+          data.products.map(item => {
+          return(
+            <div className='productcardwrapper'>
+              <img src={item.icon} className='producticon'/>
+              <div className='productdescription'>
+                  <div className='untildate'>até {item.untilDate}</div>
+                  <div className='productondiscount'>{item.name}</div>
+                  <div className='units'>
+                    <div className='productunits'>1 kg</div>
+                    <span className='dot'>·</span>
+                    <div className='priceperunit'>0.89/kg</div>
+                  </div>
+                  <div className='productprice'>
+                    <div className='newproductprice'>0.89€</div>
+                    <div className='oldproductprice'>1.05€</div>
+                  </div>
+              </div>
+              <div className='supermarketroundicon'></div>
+              <button className="addbutton">Adicionar<img src='./addgreen.png' className='plusbutton'/></button>
             </div>
-            <div className='productprice'>
-              <div className='newproductprice'></div>
-              <div className='oldproductprice'></div>
-            </div>
-        </div>
-        <div className='supermarketicon'></div>
-        <button className="addbutton">Adicionar</button>
-      </div>
-
-    )
-  }
+          )
+      })
+    }
+    </div>
+  )}
 }
+
 export default ProductCard;
